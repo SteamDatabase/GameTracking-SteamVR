c3eac3d7c5e5c308e6bfedac18e6d990002e0298
Wendy <freeman@steamdb.info>
2017-06-04 22:18:29 +0000

    1496358520 | 157 files | A  BuildbotPaths/libMilesX64.txt, A  BuildbotPaths/libanimationsystem.txt, A  BuildbotPaths/libclient.txt, A  BuildbotPaths/libengine2.txt, A  BuildbotPaths/libfilesystem_stdio.txt, A  BuildbotPaths/libhost.txt, A  BuildbotPaths/libicui18n.txt, A  BuildbotPaths/libicuuc.txt, A  BuildbotPaths/libinputsystem.txt, A  BuildbotPaths/liblocalize.txt, A  BuildbotPaths/libmaterialsystem2.txt, A  BuildbotPaths/libmeshsystem.txt, A  BuildbotPaths/libnetworksystem.txt, A  BuildbotPaths/libopenvr_api.txt, A  BuildbotPaths/libpanorama.txt, A  BuildbotPaths/libpanorama_text_pango.txt, A  BuildbotPaths/libpanoramauiclient.txt, A  BuildbotPaths/libparticles.txt, A  BuildbotPaths/librenderingpipelines.txt, A  BuildbotPaths/librendersystemempty.txt, A  BuildbotPaths/librendersystemgl.txt, A  BuildbotPaths/librendersystemvulkan.txt, A  BuildbotPaths/libresourcesystem.txt, A  BuildbotPaths/libscaleformui_4_gl.txt, A  BuildbotPaths/libscenefilecache.txt, A  BuildbotPaths/libscenesystem.txt, A  BuildbotPaths/libschemasystem.txt, A

--- update.sh
+++ update.sh
@@ -5,14 +5,8 @@ cd "${0%/*}"
 
 echo "Processing SteamVR..."
 
-#ProcessDepot ".dylib"
+ProcessDepot ".so"
 ProcessVPK
-
-while IFS= read -r -d '' file
-do
-	mono ../.support/SourceDecompiler/Decompiler.exe -i "$file" -o "$(echo "$file" | sed -e 's/\.vpk$/\//g')"
-done <   <(find . -type f -name "*_dir.vpk" -print0)
-
 FixUCS2
 
 CreateCommit "$(cat bin/version.txt | grep -o '[0-9\.]*')"
