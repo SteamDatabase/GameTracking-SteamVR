f91abb3f57b83691c021ae66d881427a2aef1dd8
Wendy <freeman@steamdb.info>
2025-06-10 18:03:20 +0000

    delete old files

--- update.sh
+++ update.sh
@@ -14,9 +14,9 @@ ProcessToolAssetInfo
 FixUCS2
 
 # Prettify js/css
-rm -r resources/webinterface/dashboard_prettier/
-rsync -av --include="*/" --include="*.js" --include="*.css" --exclude="*" --prune-empty-dirs resources/webinterface/dashboard/ resources/webinterface/dashboard_prettier/
-npm run prettier -- --write resources/webinterface/dashboard_prettier/
+rm --recursive resources/webinterface/dashboard_prettier/ || true
+rsync --archive --include="*/" --include="*.js" --include="*.css" --exclude="*" --prune-empty-dirs resources/webinterface/dashboard/ resources/webinterface/dashboard_prettier/
+npm run prettier -- --write resources/webinterface/dashboard_prettier/ || true
 
 CreateCommit "$(grep -o '[0-9\.]*' bin/version.txt)" "$1"
 
